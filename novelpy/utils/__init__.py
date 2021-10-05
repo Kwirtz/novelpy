5fff4976c54f294de6b1f1d17651da6f809d29f3
PPelletier <35504815+P-Pelletier@users.noreply.github.com>
2021-10-05 13:13:04 +0200

    lol

--- novelpy/utils/__init__.py
+++ novelpy/utils/__init__.py
@@ -2,7 +2,7 @@ from .cooc_utils import *
 from .data_utils import *
 from .run_indicator_tools import *
 from .references import *
-from .embedding import *
+#from .embedding import *
 from .Wang2017_utils import *
 from .Uzzi2013_utils import *
 from .indicators_utils import *
