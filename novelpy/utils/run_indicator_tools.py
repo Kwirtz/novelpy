ff2a6f3293854abaf20640e9105869d38413d32b
Kevin Wirtz <kevin-w@hotmail.fr>
2021-12-22 20:57:22 +0100

    Update run_indicator_tools.py
    add year for mongo

--- novelpy/utils/run_indicator_tools.py
+++ novelpy/utils/run_indicator_tools.py
@@ -278,7 +278,6 @@ class create_output(Dataset):
             score = {'novelty': float(np.mean(scores_list))}
 
         doc_infos = {"scores_array": scores_list,
-                     "year":self.focal_year,
                      'score':score}
         
         self.key = key
@@ -342,7 +341,8 @@ class create_output(Dataset):
             
             if self.client_name:
                 list_of_insertion.append(pymongo.UpdateOne({self.id_variable: int(idx)},
-                                                           {'$set': {self.key: self.doc_infos}},
+                                                           {'$set': {self.key: self.doc_infos,
+                                                                     self.year_variable:self.focal_year}}
                                                            upsert = True))
             else:
                 list_of_insertion.append({self.id_variable: int(idx),self.key: self.doc_infos})
