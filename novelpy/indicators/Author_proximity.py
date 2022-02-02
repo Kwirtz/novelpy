41607a069bc7750bf3b9a302ef7d7311bca60227
PPelletier <35504815+P-Pelletier@users.noreply.github.com>
2022-02-02 08:23:42 +0100

    Update Author_proximity.py

--- novelpy/indicators/Author_proximity.py
+++ novelpy/indicators/Author_proximity.py
@@ -64,13 +64,14 @@ class Author_proximity(Dataset):
                  id_variable = None,
                  year_variable = None,
                  aut_profile_variable = None,
+                 aut_id_variable = None,
                  entity = None,
                  focal_year = None,
                  windows_size = 5):
         """
         Description
         -----------
-        Compute Shibayama et al (2021) novelty indicator and our alternative with author poximity
+        Compute Author proximity as presented in Pelletier and Wirtz (2022)
 
         Parameters
         ----------
@@ -78,6 +79,8 @@ class Author_proximity(Dataset):
             identifier variable name.
         aut_profile_variable : str
             embedded representation of author articles variable name.
+        aut_id_variable : str
+            author id variable name.
         year_variable : str
             year variable name.
         entity : list
@@ -94,6 +97,7 @@ class Author_proximity(Dataset):
         """
         self.id_variable = id_variable
         self.aut_profile_variable = aut_profile_variable
+        self.aut_id_variable = aut_id_variable
         self.year_variable = year_variable
         self.entity = entity
         self.windows_size = windows_size 
@@ -111,6 +115,80 @@ class Author_proximity(Dataset):
         self.path_score = "Data/score/Author_proximity/"
         if not os.path.exists(self.path_score):
             os.makedirs(self.path_score)
+            
+            
+    def get_intra_dist(self,ent,doc):
+        """
+        
+
+        Parameters
+        ----------
+        ent : str
+            Entity used, can be title_profile or abstract_profile.
+        doc : dict
+            Treated document.
+
+        Returns
+        -------
+        None.
+
+        """
+        self.focal_paper_id = doc[self.id_variable]
+        nb_aut = len(doc[self.aut_profile_variable])
+        self.intra_authors_dist = []
+        self.authors_infos = []
+        # save intra authors info
+        self.authors_info_percentiles = {}
+
+        for i in range(nb_aut):
+            items = doc[self.aut_profile_variable][i][ent]
+            aut_id = doc[self.aut_profile_variable][i][self.aut_id_variable]
+            if items:
+                aut_item = [items[key] for key in items if int(key) > (doc[self.year_variable]-self.windows_size) ]
+                aut_item = list(itertools.chain.from_iterable(aut_item))
+                aut_item = [item for item in aut_item if item]
+                
+                n = len(aut_item)
+                if n >1:
+                    self.authors_infos.append(aut_item)
+                    aut_mat = np.zeros((n, 200))
+                    for j in range(n):
+                        aut_mat[j, :] = aut_item[j]
+                    aut_dist = cosine_similarity_dist(n,aut_mat)
+                    
+                    self.authors_info_percentiles.update(
+                        {
+                            str(aut_id) : get_percentiles(aut_dist)
+                            }
+                        )
+                    self.intra_authors_dist += aut_dist
+                    
+    def get_inter_dist(self):
+        
+        self.inter_authors_dist = []
+        self.authors_infos_dist = {}
+        # for all author
+        for i in range(self.nb_aut):
+            id_i = self.aut_ids[i]
+            # for all other authors
+            for j in range(i+1,self.nb_aut):
+                temp_list = []
+                id_j = self.aut_ids[j]
+                # take each paper
+                for item in self.authors_infos[i]:
+                    # compare it with all papers of author j
+                    for j_item in self.authors_infos[j]:
+                        comb = np.array([item,j_item])
+                        inter_paper_dist = cosine_similarity(comb)[0,1]
+                        temp_list.append(inter_paper_dist)
+                        self.inter_authors_dist.append(inter_paper_dist)
+                        
+                self.authors_infos_dist.update(
+                    {
+                        '{}_{}'.format(id_i,id_j) : get_percentiles(temp_list)
+                        }
+                    )
+                
 
     def compute_score(self,doc,entity):
         """
@@ -129,58 +207,31 @@ class Author_proximity(Dataset):
 
         """
 
+        self.infos = dict()
         for ent in self.entity:
 
-            self.focal_paper_id = doc[self.id_variable]
-            nb_aut = len(doc[self.aut_profile_variable])
-            authors_mat = np.zeros((nb_aut, 200))
-            intra_authors_dist = []
-            authors_infos = []
-            
-            for i in range(nb_aut):
-                items = doc[self.aut_profile_variable][i][ent]
-                if items or len(items) > 1:
+            self.get_intra_dist(ent,doc)
 
-                    aut_item = [items[key] for key in items if int(key) > (doc[self.year_variable]-self.windows_size) ]
-                    aut_item = list(itertools.chain.from_iterable(aut_item))
-                    aut_item = [item for item in aut_item if item]
-                    authors_infos.append(aut_item)
-                    
-                    n = len(aut_item)
-                    if n >1:
-                        aut_mat = np.zeros((n, 200))
-                        for i in range(n):
-                            aut_mat[i, :] = aut_item[i]
-                                    
-                        aut_dist = cosine_similarity_dist(n,aut_mat)
-                        intra_authors_dist += aut_dist
-            if len(intra_authors_dist) > 0:
-                intra_nov_list = get_percentiles(intra_authors_dist)    
-                inter_authors_dist = []
-                nb_cap = len(authors_infos)
+            if len(self.intra_authors_dist) > 0:
+                intra_nov_list = get_percentiles(self.intra_authors_dist) 
                 
-                # for all author
-                for i in range(nb_cap):
-                    # take each paper
-                    for item in authors_infos[i]:
-                        # for all other authors
-                        for j in range(i,nb_cap):
-                            # compare it with all papers
-                            for j_item in authors_infos[j]:
-                                comb = np.array([item,j_item])
-                                inter_paper_dist = cosine_similarity(comb)[0,1]
-                                inter_authors_dist.append(inter_paper_dist)
+                self.aut_ids = list(self.authors_info_percentiles.keys())    
+                self.nb_aut = len(self.aut_ids)
+                
+                if self.nb_aut > 1:
+                    self.get_inter_dist()
+                    inter_nov_list = get_percentiles(self.inter_authors_dist)
                         
-                inter_nov_list = get_percentiles(inter_authors_dist)
-                    
-                authors_novelty = {
-                    'authors_novelty_{}_{}'.format(ent, str(self.windows_size)) :{
-                        'intra':intra_nov_list,
-                        'inter':inter_nov_list,
-                        'scores_array_intra':intra_authors_dist,
-                        'scores_array_inter':inter_authors_dist}
-                    }
-                self.infos.update(authors_novelty)
+                    authors_novelty = {
+                        'authors_novelty_{}_{}'.format(ent, str(self.windows_size)) :{
+                            'intra':intra_nov_list,
+                            'inter':inter_nov_list,
+                            'scores_array_intra':self.intra_authors_dist,
+                            'scores_array_inter':self.inter_authors_dist,
+                            'individuals_scores': self.authors_info_percentiles,
+                            'iter_individuals_scores':self.authors_infos_dist}
+                        }
+                    self.infos.update(authors_novelty)
 
 
     def get_indicator(self):
@@ -194,17 +245,23 @@ class Author_proximity(Dataset):
             self.docs = json.load(open("Data/docs/{}/{}.json".format(self.collection_name,self.focal_year)))
         # Iterate over every docs 
         list_of_insertion = []
+        
         for doc in tqdm.tqdm(self.docs):
-            self.infos = dict()
-            if doc[self.aut_profile_variable] and len(doc[self.aut_profile_variable])>1: 
+            if doc[self.aut_profile_variable]: 
                 self.compute_score(doc, self.entity)
+                
                 if self.infos:
                     if self.client_name:
-                        list_of_insertion.append(pymongo.UpdateOne({self.id_variable: doc[self.id_variable]},
-                                                                   {'$set': {'Author_proximity': self.infos}},
-                                                                   upsert = True))
+                        list_of_insertion.append(
+                            pymongo.UpdateOne({self.id_variable: doc[self.id_variable]},
+                                              {'$set': {'Author_proximity': self.infos}},
+                                              upsert = True))
                     else:
-                        list_of_insertion.append({self.id_variable: doc[self.id_variable],'Author_proximity': self.infos})
+                        list_of_insertion.append(
+                            {
+                                self.id_variable: doc[self.id_variable],'Author_proximity': self.infos
+                                }
+                            )
             
         if self.client_name:
             if "output" not in self.db.list_collection_names():
