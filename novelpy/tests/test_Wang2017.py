25636e2cd85874ad70cd73bcec5e2fab369cc846
PPelletier <35504815+P-Pelletier@users.noreply.github.com>
2022-08-09 13:53:27 +0200

    up test wang2017

--- novelpy/tests/test_Wang2017.py
+++ novelpy/tests/test_Wang2017.py
@@ -9,24 +9,73 @@ import novelpy
 import unittest
 import math
 import networkx as nx
+from scipy.sparse import csr_matrix, lil_matrix, triu, tril
 
 class TestWang(unittest.TestCase):
         
     def get_papers_items(self):
         self.wang = novelpy.Wang2017(collection_name = 'Ref_journals',
-                                            id_variable = 'id',
-                                             year_variable = 'year',
-                                             variable = 'Ref_journals',
-                                             sub_variable = 'item',
-                                             focal_year = 3)
-        self.papers_item = docs3 = {5:[{"A", "C", "D"}],#NEWCOMB
-                           6:[{"B", "C", "B"}]
+                                       id_variable = 'id',
+                                       year_variable = 'year',
+                                       variable = "Ref_journals",
+                                       sub_variable = "item",
+                                       focal_year = 3,
+                                       time_window_cooc = 2,
+                                       starting_year = 1,
+                                       n_reutilisation = 1)
+        self.papers_item = docs3 = {5:["A", "C", "D"],#NEWCOMB
+                           6:["B", "C", "B"]
                           }
 
     def test_get_data(self):
         self.get_papers_items()
         self.wang.get_data()
 
-     
 
         self.assertDictEqual(d1 = self.wang.papers_items,d2 = self.papers_item)
+
+
+    def test_adj(self):
+        self.get_papers_items()
+        self.wang.get_data()
+
+        difficulty_adj = np.array([[0,3,2,0],
+                            [0,0,3,1],
+                            [0,0,0,1],
+                            [0,0,0,0]])
+
+        past_adj = np.array([[0,3,2,0],
+                            [0,0,3,1],
+                            [0,0,0,1],
+                            [0,0,0,0]])
+
+        futur_adj = np.array([[0,1,2,1],
+                            [0,0,1,1],
+                            [0,0,0,2],
+                            [0,0,0,0]])
+
+        np.testing.assert_array_equal(difficulty_adj,self.wang.difficulty_adj.A)
+        np.testing.assert_array_equal(past_adj,self.wang.past_adj.A)
+        np.testing.assert_array_equal(futur_adj,self.wang.futur_adj.A)
+
+    def test_compute_comb_score(self):
+        #cos_sim = get_difficulty_cos_sim(self.difficulty_adj)
+        #comb_scores = self.futur_adj.multiply(nbd_adj).multiply(cos_sim)
+
+        self.get_papers_items()
+        self.wang.get_data()
+        self.wang.compute_comb_score()
+
+        nbd_adj = np.array([[0,0,0,1],
+                            [0,0,0,0],
+                            [0,0,0,0],
+                            [0,0,0,0]])
+
+        futur_adj = np.array([[0,1,1,1],
+                            [0,0,1,1],
+                            [0,0,0,1],
+                            [0,0,0,0]])
+
+
+        np.testing.assert_array_equal(nbd_adj,self.wang.nbd_adj.A)
+        np.testing.assert_array_equal(futur_adj,self.wang.futur_adj.A)
