74b8cc51e9b482d74376248633a582a7f04a49b8
suryaramak <130187024+suryaramak@users.noreply.github.com>
2024-02-12 18:02:05 -0500

    Changed dependencies
    Changed dependency versions to avoid depreciation issues

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
   classifiers=classifiers,
   keywords='Novelty, scientometrics', 
   packages=find_packages(),
-  install_requires=['pymongo==3.12.1','joblib==1.1.0','sklearn==1.2.1','glob2==0.7','tqdm==4.62.3',"networkx==2.5.1",'python-louvain==0.15',
-  'numpy==1.19.5','pickle-mixin==1.0.2','scipy==1.5.4','pandas==1.1.5','multiprocess==0.70.12.2','pyyaml==5.4.1','spacy==3.0.0','scispacy==0.4.0','cycler==0.11.0','thinc==7.4.1',
+  install_requires=['pymongo==3.12.1','joblib==1.1.1','scikit-learn==1.3.0','glob2==0.7','tqdm==4.62.3',"networkx==2.5.1",'python-louvain==0.15',
+  'numpy==1.19.5','pickle-mixin==1.0.2','scipy==1.5.4','pandas==1.1.5','multiprocess==0.70.12.2','pyyaml==5.4.1','spacy==3.0.0','scispacy==0.4.0','cycler==0.11.0','thinc',
   'wosfile==0.5','seaborn==0.11.2'] 
 )
